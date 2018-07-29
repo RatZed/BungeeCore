034af897ecbdbd173a329ad8f00aec67ad4f6c5a
RatZed <41837802+RatZed@users.noreply.github.com>
2018-07-29 18:02:07 +0200

    Initial commit

---
+++ src/main/java/de/ratzed/bungeecore/BungeeCore.java
@@ -0,0 +1,14 @@
+package de.ratzed.bungeecore;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class BungeeCore extends Plugin {
+
+    public void onEnable() {
+        System.out.print("Plugin BungeeCore enabled");
+    }
+
+    public void onDisable() {
+        System.out.print("Plugin BungeeCore disabled");
+    }
+}
